aaf737aa609aa77eee828c00aa983d4362cdd515
rasha0120 <84634357+rasha0120@users.noreply.github.com>
2026-09-19 15:14:28 +0300

    Add files via upload
    OPTOacademy

---
+++ streamlit _app.py
@@ -0,0 +1,6 @@
+import streamlit _app.py
+
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
